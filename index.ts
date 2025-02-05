c6913e7c205f65a8a9b30314674466bf24e2d9ba
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-02-05 09:31:02 -0300

    mongoose

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/produtos', async (req, res) => {
-  const produtos = await Produto.find().lean()
+  const produtos = await Produto.find()
   res.send(produtos)
 })
 
