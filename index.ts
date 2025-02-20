d4f3a8457928deac7518191ec431481dbac9a8d7
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-02-19 23:02:20 -0300

    documentação com swagger

--- index.ts
+++ index.ts
@@ -5,17 +5,98 @@ import Produto from './classes/produto_mongo'
 import express from 'express'
 import cors from 'cors'
 
+
 const app = express()
 const PORTA = process.env.PORTA
 
 app.use(cors())
 app.use(express.json());
 
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Aula API',
+      version: '1.0.0',
+      description: 'Documentação da API usando Swagger',
+    },
+    servers: [{ url: 'http://localhost:3000' }],
+  },
+  apis: [
+    './index.ts'
+  ], // Define onde os comentários da API estão
+}
+
+const swaggerDocs = swaggerJsDoc(options)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Produto:
+ *       type: object
+ *       required:
+ *         - nome
+ *       properties:
+ *         nome:
+ *           type: string
+ *           description: Nome do produto
+ *         valor:
+ *           type: number
+ *           description: Valor do produto
+ *       example:
+ *         nome: "Produto Exemplo"
+ *         valor: 100
+ */
+
+/**
+ * @swagger
+ * /produtos:
+ *   get:
+ *     summary: Retorna a lista de todos os produtos
+ *     tags: [Produtos]
+ *     responses:
+ *       200:
+ *         description: Lista de produtos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Produto'
+ */
 app.get('/produtos', async (req, res) => {
   const produtos = await Produto.find()
   res.send(produtos)
 })
 
+/**
+ * @swagger
+ * /produtos:
+ *   post:
+ *     summary: Cria um novo produto
+ *     tags: [Produtos]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Produto'
+ *     responses:
+ *       200:
+ *         description: Produto criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Produto'
+ *       500:
+ *         description: Erro ao criar o produto
+ */
 app.post('/produtos', async (req, res) => {
   try {
   const produto = await Produto.create({
@@ -30,6 +111,29 @@ app.post('/produtos', async (req, res) => {
   }
 })
 
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   get:
+ *     summary: Retorna um produto pelo ID
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do produto
+ *     responses:
+ *       200:
+ *         description: Produto encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Produto'
+ *       404:
+ *         description: Produto não encontrado
+ */
 app.get('/produtos/:id', async (req, res) => {
   const produto = await Produto.findById(req.params.id)
   if (produto) {
@@ -39,6 +143,31 @@ app.get('/produtos/:id', async (req, res) => {
   }
 })
 
+/**
+ * @swagger
+ * /produtos/search/{nome}:
+ *   get:
+ *     summary: Busca produtos pelo nome
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: nome
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Nome do produto
+ *     responses:
+ *       200:
+ *         description: Produtos encontrados
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Produto'
+ *       404:
+ *         description: Nenhum produto encontrado
+ */
 app.get('/produtos/search/:nome', async (req, res) => {
   const produtos = await Produto.find({
     nome: {
@@ -49,6 +178,37 @@ app.get('/produtos/search/:nome', async (req, res) => {
   res.send(produtos)
 })
 
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   put:
+ *     summary: Atualiza um produto pelo ID
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do produto
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Produto'
+ *     responses:
+ *       200:
+ *         description: Produto atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Produto'
+ *       404:
+ *         description: Produto não encontrado
+ *       500:
+ *         description: Erro ao atualizar o produto
+ */
 app.put('/produtos/:id', async (req, res) => {
   // Opção 1: Buscar e atualizar manualmente
   // const produto = await Produto.findById(req.params.id)
@@ -70,7 +230,27 @@ app.put('/produtos/:id', async (req, res) => {
   res.sendStatus(200)
 })
 
-// Deletar um produto pelo ID
+/**
+ * @swagger
+ * /produtos/{id}:
+ *   delete:
+ *     summary: Deleta um produto pelo ID
+ *     tags: [Produtos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do produto
+ *     responses:
+ *       200:
+ *         description: Produto deletado com sucesso
+ *       404:
+ *         description: Produto não encontrado
+ *       500:
+ *         description: Erro ao deletar o produto
+ */
 app.delete('/produtos/:id', async (req, res) => {
   // // Não retorna o documento removido
   // await Produto.deleteOne({_id: req.params.id})
@@ -80,6 +260,26 @@ app.delete('/produtos/:id', async (req, res) => {
   res.status(200).send()
 })
 
+/**
+ * @swagger
+ * /relatorios/soma_valores:
+ *   get:
+ *     summary: Retorna a soma dos valores de todos os produtos
+ *     tags: [Relatórios]
+ *     responses:
+ *       200:
+ *         description: Soma dos valores dos produtos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 valorTotal:
+ *                   type: number
+ *                   description: Soma dos valores dos produtos
+ *       500:
+ *         description: Erro ao gerar o relatório
+ */
 app.get('/relatorios/soma_valores', async (requestAnimationFrame, res) => {
   try {
     const resultado = await Produto.aggregate([
@@ -94,6 +294,26 @@ app.get('/relatorios/soma_valores', async (requestAnimationFrame, res) => {
   }
 })
 
+/**
+ * @swagger
+ * /relatorios/qtde_produtos:
+ *   get:
+ *     summary: Retorna a quantidade total de produtos
+ *     tags: [Relatórios]
+ *     responses:
+ *       200:
+ *         description: Quantidade total de produtos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 quantidade:
+ *                   type: number
+ *                   description: Quantidade total de produtos
+ *       500:
+ *         description: Erro ao gerar o relatório
+ */
 app.get('/relatorios/qtde_produtos', async (requestAnimationFrame, res) => {
   try {
     const resultado = await Produto.aggregate([
